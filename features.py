9035ea2817242ed064578aa579fbf74b98b7d347
afonso <ajcerejeira@gmail.com>
2017-12-29 11:47:04 +0000

    Started features script

--- features.py
+++ features.py
@@ -3,3 +3,13 @@
 """
 Reads the features from a sequence, and exports them to plain text or HTML.
 """
+
+from Bio import SeqIO
+
+GENOME = 'genome.gb'
+SEQUENCE = 'sequence.txt'
+
+record = SeqIO.read(GENOME, 'genbank')
+
+for feature in record.features:
+    print(feature)
